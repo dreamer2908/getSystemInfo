50ad4597f60188b8405aae81e18f0d9e01ebe061
dreamer2908 <dreamer2908@gmail.com>
2019-10-19 15:00:38 +0700

    systemResourceMonitor - a lot of changes Downgrade to .NET 4.0 to support XP & Server 2003 Remove needless references sbAppendWriteLine to also write to console Various fixes

--- systemResourceMonitor_cli/Program.cs
+++ systemResourceMonitor_cli/Program.cs
@@ -3,19 +3,18 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Net.NetworkInformation;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace systemResourceMonitor_cli
 {
     class Program
     {
+        static StringBuilder sb = new StringBuilder();
+
         static void Main(string[] args)
         {
-            StringBuilder sb = new StringBuilder();
 
             //PerformanceCounter cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
             //float t = cpuCounter.NextValue(); // the first reading is always 0
@@ -27,37 +26,51 @@ namespace systemResourceMonitor_cli
             //    Console.WriteLine(t);
             //}
 
-            sb.AppendLine("System Time: " + getNowString());
+            sbAppendWriteLine("System Time: " + getNowString());
 
             #region System
-            sb.AppendLine(string.Format("Mainboard Manufacturer: {0}", systemInfo.getMobo_manufacturer()));
-            sb.AppendLine(string.Format("Mainboard Model: {0}", systemInfo.getMobo_model()));
+            sbAppendWriteLine("Mainboard Manufacturer: {0}", systemInfo.getMobo_manufacturer());
+            sbAppendWriteLine("Mainboard Model: {0}", systemInfo.getMobo_model());
 
-            sb.AppendLine(string.Format("System Manufacturer: {0}", systemInfo.getSystem_manufacturer()));
-            sb.AppendLine(string.Format("System Model: {0}", systemInfo.getSystem_model()));
+            sbAppendWriteLine("System Manufacturer: {0}", systemInfo.getSystem_manufacturer());
+            sbAppendWriteLine("System Model: {0}", systemInfo.getSystem_model());
 
             // Console.WriteLine(misc.byteToHumanSize(systemInfo.getSystem_systemDriveFreeSpace()));
             // Console.WriteLine(misc.byteToHumanSize(systemInfo.getSystem_systemDriveTotalSpace()));
 
-            sb.AppendLine(string.Format("System OS: {0}", systemInfo.getSystem_OS()));
-            sb.AppendLine(string.Format("System Language: {0}", systemInfo.getSystem_language()));
-            sb.AppendLine(string.Format("Computer Name: {0}", systemInfo.getSystem_name()));
-            sb.AppendLine(string.Format("Domain Name: {0}", systemInfo.getSystem_domain()));
-            sb.AppendLine(string.Format("Current Username: {0}", systemInfo.getSystem_currentUsername()));
+            sbAppendWriteLine("System OS: {0}", systemInfo.getSystem_OS());
+            sbAppendWriteLine("System Language: {0}", systemInfo.getSystem_language());
+            sbAppendWriteLine("Computer Name: {0}", systemInfo.getSystem_name());
+            sbAppendWriteLine("Domain Name: {0}", systemInfo.getSystem_domain());
+
+            sbAppendWriteLine("Current Username: {0}", System.Security.Principal.WindowsIdentity.GetCurrent().Name);
             #endregion
 
             #region CPU
-            sb.AppendLine(string.Format("CPU Name: {0}", systemInfo.getCPU_name()));
-            sb.AppendLine(string.Format("CPU Core Count: {0}", systemInfo.getCPU_coreCount()));
-            sb.AppendLine(string.Format("CPU Thread Count: {0}", systemInfo.getCPU_threadCount()));
-            sb.AppendLine(string.Format("CPU Clock Speed: {0}", systemInfo.getCPU_clockSpeed()));
+            var allCPUs = systemInfo.getCPU_all();
+            sbAppendWriteLine("CPU Socket Count: {0}", allCPUs.Count);
+            for (int i = 0; i < allCPUs.Count; i++)
+            {
+                sbAppendWriteLine("CPU #{0}", i);
+                sbAppendWriteLine("    CPU Name: {0}", allCPUs[i][0]);
+                sbAppendWriteLine("    CPU Core Count: {0}", allCPUs[i][1]);
+                sbAppendWriteLine("    CPU Thread Count: {0}", allCPUs[i][2]);
+                sbAppendWriteLine("    CPU Clock Speed: {0}", allCPUs[i][3]);
+            }
 
-            PerformanceCounter cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
-            float t = cpuCounter.NextValue(); // the first reading is always 0
-            System.Threading.Thread.Sleep(1000); // wait a second to get a valid reading
-            t = cpuCounter.NextValue(); // this time it will be correct
+            try
+            {
+                PerformanceCounter cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+                float t = cpuCounter.NextValue(); // the first reading is always 0
+                System.Threading.Thread.Sleep(1000); // wait a second to get a valid reading
+                t = cpuCounter.NextValue(); // this time it will be correct
 
-            sb.AppendLine(string.Format("CPU Usage: {0:0.00}%", t));
+                sbAppendWriteLine("CPU Usage: {0:0.00}%", t);
+            }
+            catch (System.InvalidOperationException)
+            {
+                sbAppendWriteLine("CPU Usage: N/A");
+            }
             #endregion
 
             #region RAM
@@ -67,22 +80,23 @@ namespace systemResourceMonitor_cli
             long used = total - available;
             double usedpercent = 100.0 * used / total;
             double availablepercent = 100.0 * available / total;
-            sb.AppendLine(string.Format("RAM Total Capacity: {0}", misc.byteToHumanSize(total)));
-            sb.AppendLine(string.Format("RAM Used Size: {0} ({1:0.00}%)", misc.byteToHumanSize(used), usedpercent));
-            sb.AppendLine(string.Format("RAM Free Size: {0} ({1:0.00}%)", misc.byteToHumanSize(available), availablepercent));
+            sbAppendWriteLine("RAM Total Capacity: {0}", misc.byteToHumanSize(total));
+            sbAppendWriteLine("RAM Used Size: {0} ({1:0.00}%)", misc.byteToHumanSize(used), usedpercent);
+            sbAppendWriteLine("RAM Free Size: {0} ({1:0.00}%)", misc.byteToHumanSize(available), availablepercent);
 
-            sb.AppendLine(string.Format("RAM Slot Count: {0}", systemInfo.getRAM_slotCount()));
-            sb.AppendLine(string.Format("RAM Stick Count: {0}", systemInfo.getRAM_stickCount()));
+            sbAppendWriteLine("RAM Slot Count: {0}", systemInfo.getRAM_slotCount());
+            sbAppendWriteLine("RAM Stick Count: {0}", systemInfo.getRAM_stickCount());
 
             List<string[]> RAMs = systemInfo.getRAM_stickList();
 
             for (int i = 0; i < RAMs.Count; i++)
             {
-                sb.AppendLine(string.Format("RAM Stick #{0}:", i + 1));
-                sb.AppendLine(string.Format("    Manufacturer: {0}", RAMs[i][0]));
-                sb.AppendLine(string.Format("    PartNumber: {0}", RAMs[i][1]));
-                sb.AppendLine(string.Format("    Capacity: {0}", misc.byteToHumanSize(long.Parse(RAMs[i][2]))));
-                sb.AppendLine(string.Format("    Bus Speed: {0} MHz", RAMs[i][3]));
+                sbAppendWriteLine("RAM Stick #{0}:", i + 1);
+                sbAppendWriteLine("    Manufacturer: {0}", RAMs[i][0]);
+                sbAppendWriteLine("    PartNumber: {0}", RAMs[i][1]);
+                sbAppendWriteLine("    Capacity: {0}", misc.byteToHumanSize(long.Parse(RAMs[i][2])));
+                sbAppendWriteLine("    Bus Speed: {0} MHz", RAMs[i][3]);
+                sbAppendWriteLine("    Type: {0}", RAMs[i][4]);
             }
             #endregion
 
@@ -90,35 +104,36 @@ namespace systemResourceMonitor_cli
 
             DriveInfo[] allDrives = DriveInfo.GetDrives();
 
-            sb.AppendLine(string.Format("Logical Drive Count: {0}", allDrives.Length));
+            sbAppendWriteLine("Logical Drive Count: {0}", allDrives.Length);
 
             foreach (DriveInfo d in allDrives)
             {
-                sb.AppendLine(string.Format("    Drive {0}", d.Name));
-                sb.AppendLine(string.Format("        Drive type: {0}", d.DriveType));
+                sbAppendWriteLine("    Drive {0}", d.Name);
+                sbAppendWriteLine("        Drive type: {0}", d.DriveType);
                 if (d.IsReady == true)
                 {
-                    sb.AppendLine(string.Format("        Volume label: {0}", d.VolumeLabel));
-                    sb.AppendLine(string.Format("        File system: {0}", d.DriveFormat));
-                    sb.AppendLine(string.Format("        Available space to current user: {0}", misc.byteToHumanSize(d.AvailableFreeSpace)));
-                    sb.AppendLine(string.Format("        Total available space: {0}", misc.byteToHumanSize(d.TotalFreeSpace)));
-                    sb.AppendLine(string.Format("        Total size of drive: {0} ", misc.byteToHumanSize(d.TotalSize)));
+                    sbAppendWriteLine("        Volume label: {0}", d.VolumeLabel);
+                    sbAppendWriteLine("        File system: {0}", d.DriveFormat);
+                    sbAppendWriteLine("        Available space to current user: {0}", misc.byteToHumanSize(d.AvailableFreeSpace));
+                    sbAppendWriteLine("        Total available space: {0}", misc.byteToHumanSize(d.TotalFreeSpace));
+                    sbAppendWriteLine("        Total size of drive: {0} ", misc.byteToHumanSize(d.TotalSize));
                 }
             }
 
             var HDDs = systemInfo.getHDD_list();
-            sb.AppendLine(string.Format("Physical HDD Count: {0}", HDDs.Count));
+            sbAppendWriteLine("Physical HDD Count: {0}", HDDs.Count);
 
             for (int i = 0; i < HDDs.Count; i++)
             {
-                sb.AppendLine(string.Format("HDD #{0}:", i + 1));
+                sbAppendWriteLine("HDD #{0}:", i + 1);
 
                 var HDD = HDDs[i];
 
-                sb.AppendLine("    Address: " + HDD.addr);
-                sb.AppendLine("    Model: " + HDD.model);
-                sb.AppendLine("    Capacity: " + misc.byteToHumanSize(HDD.size));
-                sb.AppendLine("    S.M.A.R.T Info:\n" + padSpaceLines(HDD.smart, "        ") + "\n");
+                sbAppendWriteLine("    Address: " + HDD.addr);
+                sbAppendWriteLine("    Model: " + HDD.model);
+                sbAppendWriteLine("    Capacity: " + misc.byteToHumanSize(HDD.size));
+                sbAppendWriteLine("    S.M.A.R.T Info:");
+                sbAppendWriteLine(addPaddingToLines(HDD.smart, "        "));
             }
             #endregion
 
@@ -126,31 +141,31 @@ namespace systemResourceMonitor_cli
             var networkInterface = systemInfo.getNetwork_interfaces();
             NetworkInterface[] allNetworkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
 
-            sb.AppendLine(string.Format("Network Interface Count: {0}", networkInterface.Count));
+            sbAppendWriteLine("Network Interface Count: {0}", networkInterface.Count);
 
             for (int i = 0; i < networkInterface.Count; i++)
             {
                 var network = networkInterface[i];
-                sb.AppendLine(string.Format("Network Interface #{0}:", i + 1));
-                sb.AppendLine(string.Format("    Name: {0}", network.name));
-                sb.AppendLine(string.Format("    Description: {0}", network.description));
-                sb.AppendLine(string.Format("    MAC Address: {0}", network.MAC));
-                sb.AppendLine(string.Format("    Status: {0}", network.isUp ? "Up" : "Down"));
-                sb.AppendLine(string.Format("    Speed: {0}", misc.bpsToHumanSize(network.speed)));
-                sb.AppendLine(string.Format("    IP Addresses ({0}):", network.ipAddresses.Count));
+                sbAppendWriteLine("Network Interface #{0}:", i + 1);
+                sbAppendWriteLine("    Name: {0}", network.name);
+                sbAppendWriteLine("    Description: {0}", network.description);
+                sbAppendWriteLine("    MAC Address: {0}", network.MAC);
+                sbAppendWriteLine("    Status: {0}", network.isUp ? "Up" : "Down");
+                sbAppendWriteLine("    Speed: {0}", misc.bpsToHumanSize(network.speed));
+                sbAppendWriteLine("    IP Addresses ({0}):", network.ipAddresses.Count);
                 foreach (var ip in network.ipAddresses)
                 {
-                    sb.AppendLine(string.Format("        {0} {1}", ip.ipAddr, ip.netMask));
+                    sbAppendWriteLine("        {0} {1}", ip.ipAddr, ip.netMask);
                 }
-                sb.AppendLine(string.Format("    Gateways ({0}):", network.gateways.Count));
+                sbAppendWriteLine("    Gateways ({0}):", network.gateways.Count);
                 foreach (var gate in network.gateways)
                 {
-                    sb.AppendLine(string.Format("        {0}", gate));
+                    sbAppendWriteLine("        {0}", gate);
                 }
-                sb.AppendLine(string.Format("    DNS Servers ({0}):", network.dnsServers.Count));
+                sbAppendWriteLine("    DNS Servers ({0}):", network.dnsServers.Count);
                 foreach (var dns in network.dnsServers)
                 {
-                    sb.AppendLine(string.Format("        {0}", dns));
+                    sbAppendWriteLine("        {0}", dns);
                 }
 
                 // get the current throughput of this interface
@@ -183,11 +198,11 @@ namespace systemResourceMonitor_cli
                             double sendSpeedPerc = 100.0 * bitSentSpeed / thisInterface.Speed;
                             double recvSpeedPerc = 100.0 * bitReceivedSpeed / thisInterface.Speed;
 
-                            sb.AppendLine("    Stats:");
-                            // sb.AppendLine(string.Format("        TX Bytes: {0}", bytesSentDelta));
-                            // sb.AppendLine(string.Format("        RX Bytes: {0}", bitReceivedSpeed));
-                            sb.AppendLine(string.Format("        TX Speed: {0} ({1:0.00}%)", misc.bpsToHumanSize(bitSentSpeed), sendSpeedPerc));
-                            sb.AppendLine(string.Format("        RX Speed: {0} ({1:0.00}%)", misc.bpsToHumanSize(bitReceivedSpeed), recvSpeedPerc));
+                            sbAppendWriteLine("    Stats:");
+                            // sbAppendLine(string.Format("        TX Bytes: {0}", bytesSentDelta));
+                            // sbAppendLine(string.Format("        RX Bytes: {0}", bitReceivedSpeed));
+                            sbAppendWriteLine("        TX Speed: {0} ({1:0.00}%)", misc.bpsToHumanSize(bitSentSpeed), sendSpeedPerc);
+                            sbAppendWriteLine("        RX Speed: {0} ({1:0.00}%)", misc.bpsToHumanSize(bitReceivedSpeed), recvSpeedPerc);
                         }
 
                         break;
@@ -202,18 +217,18 @@ namespace systemResourceMonitor_cli
             // Console.ReadLine();
         }
 
-        static string padSpaceLines(string text, string pad)
+        static string addPaddingToLines(string text, string pad)
         {
-            var sb = new StringBuilder();
+            var bs = new StringBuilder();
 
             var input = text.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
 
             foreach (var line in input)
             {
-                sb.AppendLine(pad + line);
+                bs.AppendLine(pad + line);
             }
 
-            return sb.ToString();
+            return bs.ToString();
         }
 
         public static string getNowStringForFilename()
@@ -229,6 +244,18 @@ namespace systemResourceMonitor_cli
         public static string formatDateTime(DateTime d)
         {
             return d.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+
+        public static void sbAppendWriteLine(string text)
+        {
+            Console.WriteLine(text);
+            sb.AppendLine(text);
+        }
+
+        public static void sbAppendWriteLine(string text, params object[] arg)
+        {
+            Console.WriteLine(text, arg);
+            sb.AppendLine(string.Format(text, arg));
         }
     }
 }
