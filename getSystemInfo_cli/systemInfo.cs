0325191ff9d36ca3fd2baf1f08664b6259d52a55
dreamer2908 <dreamer2908@gmail.com>
2020-01-14 15:58:17 +0700

    Handle NetworkInformationException exception and whitelist Microsoft Network Adapter Multiplexor Driver

--- getSystemInfo_cli/systemInfo.cs
+++ getSystemInfo_cli/systemInfo.cs
@@ -440,6 +440,7 @@ namespace getSystemInfo_cli
             NetworkInterface[] fNetworkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
             foreach (NetworkInterface adapter in fNetworkInterfaces)
             {
+
                 string fRegistryKey = "SYSTEM\\CurrentControlSet\\Control\\Network\\{4D36E972-E325-11CE-BFC1-08002BE10318}\\" + adapter.Id + "\\Connection";
                 RegistryKey rk = Registry.LocalMachine.OpenSubKey(fRegistryKey, false);
                 if (rk != null)
@@ -449,15 +450,24 @@ namespace getSystemInfo_cli
 
                     // filter real network cards. the device instance path should have "PCI" or "USB" at the beginning or sometimes in the middle (rare)
                     // virtual interface' device instance path start with "ROOT"
-                    if (fPnpInstanceID.Length > 3 && (fPnpInstanceID.Substring(0, 3) == "PCI" || fPnpInstanceID.Substring(0, 3) == "USB" || fPnpInstanceID.Contains("PCI")))
+                    if (fPnpInstanceID.Length > 3 && (fPnpInstanceID.StartsWith("PCI") || fPnpInstanceID.StartsWith("USB") || fPnpInstanceID.Contains("PCI") || fPnpInstanceID.StartsWith("COMPOSITEBUS")))
                     {
+                        bool isDhcpEnabled;
+                        try
+                        {
+                            isDhcpEnabled = adapter.GetIPProperties().GetIPv4Properties().IsDhcpEnabled;
+                        }
+                        catch (NetworkInformationException)
+                        {
+                            isDhcpEnabled = false;
+                        }
                         sruct_networkInterfaceInfo thisOne = new sruct_networkInterfaceInfo
                         {
                             name = adapter.Name,
                             description = adapter.Description,
                             MAC = adapter.GetPhysicalAddress().ToString(),
                             isUp = (adapter.OperationalStatus == OperationalStatus.Up),
-                            isDhcpEnabled = adapter.GetIPProperties().GetIPv4Properties().IsDhcpEnabled,
+                            isDhcpEnabled = isDhcpEnabled,
                             ipAddresses = new List<struct_ipAddr>(),
                             gateways = new List<string>(),
                             dnsServers = new List<string>()
